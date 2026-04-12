a04dbd4f926afcfb9cfea99f319d50dcbaf19681
Shai Pilosof <shainova@gmail.com>
2026-04-12 08:40:18 +0300

    added code to test emln integration

---
+++ tests/test_emln.R
@@ -0,0 +1,15 @@
+devtools::document()
+devtools::check()
+devtools::load_all()
+?plot_multilayer
+?multilayer_to_json
+?multilayer_to_csv
+net <- load_emln(28)               # load a built-in dataset
+net$references
+# Open directly in the browser
+srv <- plot_multilayer(net, bipartite = TRUE)
+httpuv::stopServer(srv)            # stop server when done
+
+# Or export to JSON / CSV for manual upload
+multilayer_to_json(net, file = "net60.json", bipartite = TRUE)
+multilayer_to_csv(net, dir = "out/", prefix = "net60", bipartite = TRUE)
